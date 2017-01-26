a8189e8c0980389390094dab89fc09dadf725fbf
Hyang <hyang@Hyang-ui-MacBook-Air.local>
2017-01-27 06:37:10 +0900

    second commit'

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def index(request):
+    ctx = {
+    }
+    return render(request, "index.html", ctx)
+
+# def detail(request):
+#     pass
+
+# def about(request):
+#     pass
