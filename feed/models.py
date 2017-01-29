e494fb595b0b8c872ca64cdf1af00719de7759be
Hyang <hyang@Hyang-ui-MacBook-Air.local>
2017-01-29 15:49:48 +0900

    03-week-assignment

--- feed/models.py
+++ feed/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class HashTag(models.Model):
+    name = models.CharField(max_length = 50)
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
     DEVELOPMENT = 'dv'
     PERSONAL = 'ps'
@@ -10,21 +15,37 @@ class Article(models.Model):
         (PERSONAL, 'personal'),
     )
     title = models.CharField(max_length = 20)
+    sub_title = models.CharField(max_length = 50)
     content = models.TextField()
     category = models.CharField(
         max_length = 2,
         choices = CATEGORY_CHOICES,
         default = DEVELOPMENT,
     )
+
+    hashtag = models.ManyToManyField(HashTag)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def approved_comments(self):
+        return self.article_comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article)
+    article = models.ForeignKey(
+    Article,
+    related_name = "article_comments",
+    on_delete = models.CASCADE
+    )
     username = models.CharField(max_length = 50)
     comment = models.CharField(max_length = 200)
+    approved_comment = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    # def approve(self):
+    # self.approved_comment = True
+    # self.save()
+
     def __str__(self):
         return "{} 댓글: {}".format(self.article.title, self.comment)
-
-class HashTag(models.Model):
-    name = models.CharField(max_length = 50)
--- feed/views.py
+++ feed/views.py
@@ -1,14 +1,55 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Comment, HashTag
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
+    category = request.GET.get("category")
+    hashtag = request.GET.get("hashtag")
+
+    hashtag_list = HashTag.objects.all()
+
+    if not hashtag and not category:
+        article_list = Article.objects.all()
+    elif category:
+        article_list = Article.objects.filter(category = category)
+    else :
+        article_list = Article.objects.filter(hashtag__name = hashtag)
+
+    category_list = set([
+        (article.category, article.get_category_display())
+        for article in article_list
+    ])
+
     ctx = {
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list,
+        "category_list" : category_list,
     }
     return render(request, "index.html", ctx)
 
-# def detail(request):
-#     pass
+def detail(request, article_id):
 
+    article = Article.objects.get(id = article_id)
+    hashtag_list = HashTag.objects.all()
+    ctx = {
+        "article" : article,
+        "hashtag_list" : hashtag_list,
+    }
+
+    # if request.method == 'GET' :
+    #     pass
+
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        comment = request.POST.get('comment')
+        Comment.objects.create(
+            article = article,
+            username = username,
+            comment = comment
+        )
+        return HttpResponseRedirect('/{}/'.format(article_id))
+
+    return render(request, "detail.html", ctx)
 # def about(request):
 #     pass
